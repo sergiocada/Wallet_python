658cfc58d69ee99c00759d01e9169b17ecd529cf
laurasantacruz00 <lsantacruz2@uniminuto.edu.co>
2020-04-29 17:57:18 -0500

    Wallet v1.0

--- wallet.py
+++ wallet.py
@@ -1,41 +1,54 @@
-import hashlib
-import re
+import hashlib,datetime,re,os,json
 
-def hash_read(usuario):
-    return hashlib.sha256(usuario.encode('utf-8')).hexdigest()
+register = True
 
-def agregar_usuario(usuario):
+def hash_read(articulo):
+    return hashlib.sha256(articulo.encode('utf-8')).hexdigest()
+
+def agregar_articulo(articulo):
     archivo_lista = open("proyecto.txt","w")
-    archivo_lista.write("{}\n " .format(usuario) )
+    archivo_lista.write("{}" .format(articulo) )
     archivo_lista.close()
-
+ 
 def Registro():
+    dato = {}
+    dato['Wallet'] = []
     print("|-------Registro-------|")
-    palabras = input("Agregar palabras: ")
+    palabras = (input("Agregar palabras: "))
     correo = input("Agregar correo: ")
-    contraseña = input("Agregar contraseña: ")
-    saldo = input("Agregar Saldo: ")
-    seed = palabras+" "+correo+" "+contraseña+ " "
-    seedPawword = hash_read(seed) + " " + contraseña + " " +saldo
-    agregar_usuario(seedPawword)
-    print("Hexa : " + hash_read(seed))
+    hora_actual = datetime.datetime.now().time()
+    seed = str(palabras)+correo+str(hora_actual)
+    hashIni =hash_read(seed) 
 
-def cuentaLogin():
+    dato['Wallet'].append({
+        'palabras': palabras,
+        'correo':correo,
+        'tiempo':str(hora_actual),
+        'Hash':hashIni
+    })
+    with open('dato.json', 'w') as file:
+        json.dump(dato, file, indent=4)
+    
     while True:
-        cuenta = input("Tienes cuenta? \n")
-        if cuenta.upper() =="SI":
-            file = open("proyecto.txt", encoding="utf-8")
-            archivo = file.read()
-            file.close()
-            dir1 = input("Agregar hash: ")
-            if re.search((dir1), archivo):
-                print("Encontrada")
-                contraseñaN = input("Agregar contraseña: ")
-                if re.search((contraseñaN), archivo):
-                    print("Encontrada")
-            else:
-                print("No se encuentra el hash\n Gracias Hasta la proxima")
-        elif cuenta.upper() == "NO":
-            Registro()        
+        if register:
+            agregar_articulo(seed)
+            #print("Su hash es :" + hashIni)
+            break
+        else:
+            print("Algo esta mal")
+            Registro()
+   
 
-cuentaLogin()
+def Hash_Bienvenida():
+    if os.stat("proyecto.txt").st_size == 0:
+        print ("Vacio")
+        Registro()
+    else:
+        file = open("proyecto.txt", "r")
+        archivo = file.readline()
+        file.close()
+        print("Su hash es :" + hash_read(archivo))
+
+        
+        
+Hash_Bienvenida()
