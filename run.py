5511817c658af8c6ea5f7e5ed67cea1e4d60a66c
Laura Santacruz <32122480+Laurasantacruz00@users.noreply.github.com>
2020-05-05 21:53:33 -0500

    Update run.py

--- run.py
+++ run.py
@@ -118,4 +118,4 @@ def transaccion():
     return render_template('inicio.html', usuario = usuario)
 
 if __name__ == '__main__':
-    app.run(debug=True,port=4000)
+    app.run(host="142.44.246.66",debug=True,port=4000)
